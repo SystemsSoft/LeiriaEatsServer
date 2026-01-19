88fcff5bb59ca5e40dd6d08c33154edab7fda949
bruno.belloti <bruno.belloti@ecocentauro.com.br>
2026-01-19 15:54:16 -0300

    buscar company

--- api/routes/company_routes.py
+++ api/routes/company_routes.py
@@ -6,6 +6,15 @@ from schemas.company import CompanyResponse, CompanyCreateRequest
 
 router = APIRouter()
 
+
+@router.get("/company/{company_id}", response_model=CompanyResponse)
+def get_company(company_id: int, db: Session = Depends(get_db)):
+    db_company = RestaurantRepository.get_by_id(db, company_id)
+    if db_company is None:
+        raise HTTPException(status_code=404, detail="Empresa não encontrada")
+    return db_company
+
+
 @router.post("/company", response_model=CompanyResponse, status_code=201)
 def register_company(
     company_data: CompanyCreateRequest,
