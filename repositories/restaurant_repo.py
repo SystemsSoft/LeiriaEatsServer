bdb854ac688366b0b83e1d77e05d6122753c54ac
bruno.belloti <bruno.belloti@ecocentauro.com.br>
2026-02-17 09:48:21 -0300

    versaoweb

--- repositories/restaurant_repo.py
+++ repositories/restaurant_repo.py
@@ -1,14 +1,8 @@
-# Arquivo: repositories/restaurant_repo.py
 from sqlalchemy.orm import Session, joinedload
 from typing import List
 from core.sql_models import RestaurantDB, ProductDB
 from schemas.company import CompanyCreateRequest
 from schemas.product import ProductCreateRequest
-
-from passlib.context import CryptContext
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
 
 class RestaurantRepository:
 
@@ -22,11 +16,7 @@ class RestaurantRepository:
 
     @staticmethod
     def create_company(db: Session, company: CompanyCreateRequest):
-        # 1. Criptografa a senha antes de salvar
-        # Agora o 'pwd_context' existe e vai funcionar
-        hashed_password = pwd_context.hash(company.password)
 
-        # 2. Cria o objeto do banco com a senha criptografada
         db_restaurant = RestaurantDB(
             name=company.name,
             category=company.category,
@@ -36,7 +26,7 @@ class RestaurantRepository:
 
             # Novos campos
             login=company.login,
-            password=hashed_password,  # SALVA O HASH!
+            password=company.password,
             license=company.license
         )
 
@@ -45,11 +35,8 @@ class RestaurantRepository:
         db.refresh(db_restaurant)
         return db_restaurant
 
-    # Adicionei o método create_product aqui também para garantir que não falte
     @staticmethod
     def create_product(db: Session, product: ProductCreateRequest):
-        # Verifica se product.restaurant_id é válido antes de criar (opcional, mas recomendado)
-
         db_product = ProductDB(
             name=product.name,
             description=product.description,
@@ -68,10 +55,9 @@ class RestaurantRepository:
         user = db.query(RestaurantDB).filter(RestaurantDB.login == login).first()
 
         if not user:
-            return None  # Usuário não existe
+            return None
 
-        # 2. Verifica se a senha bate com o hash
-        if not pwd_context.verify(plain_password, user.password):
-            return None  # Senha incorreta
+        if plain_password != user.password:
+            return None
 
         return user
